f6234f8f322cd420fa509f9c1345de0185775f99
Melvin Idema <melvinidema@melvins-mbp.home>
2021-04-27 15:49:05 +0200

    Add Redux to TaskList

---
+++ src/components/Task.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Task({task: { id, title, state}, onArchiveTask, onPinTask}) {
+    return (
+        <div className={['list-item', state].join(' ')}>
+            <label htmlFor={'checked'} className={'checkbox'}>
+            <input
+                type="checkbox"
+                defaultChecked={state === 'TASK_ARCHIVED'}
+                disabled={true}
+                value={title}
+                name={'checked'}
+                readOnly={true} />
+            <span className={'checkbox-custom'} onClick={() => onArchiveTask(id)} />
+            </label>
+            <div className="title">
+                <input type="text" value={title} readOnly={true} placeholder={"Input title"}/>
+            </div>
+
+            <div className="actions" onClick={event => event.stopPropagation()}>
+                {state !== 'TASK_ARCHIVED' && (
+                    <a onClick={() => {onPinTask(id)}}>
+                        <span className="icon-star" />
+                    </a>
+                )}
+            </div>
+        </div>
+    )
+}
+
+Task.propTypes = {
+    /** Task composition */
+    task: PropTypes.shape({
+        /** ID of the task */
+        id: PropTypes.number.isRequired,
+        /** Title of the task */
+        title: PropTypes.string.isRequired,
+        /** Current state of the task */
+        state: PropTypes.string.isRequired,
+    }).isRequired,
+    /** Event to change the task to archived */
+    onArchiveTask: PropTypes.func,
+    /** Event to change the task to pinned */
+    onPinTask: PropTypes.func,
+};
---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { PureTaskList } from './TaskList';
+import * as TaskStories from './Task.stories';
+export default {
+    component: PureTaskList,
+    title: 'TaskList',
+    decorators: [story => <div style={{ padding: '3em' }}>{story()}</div>],
+};
+
+const Template = args => <PureTaskList {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    tasks: [
+        {...TaskStories.default.task, id: 1, title: 'Task 1'},
+        {...TaskStories.default.task, id: 2, title: 'Task 2'},
+        {...TaskStories.default.task, id: 3, title: 'Task 3'},
+        {...TaskStories.default.task, id: 4, title: 'Task 4'},
+        {...TaskStories.default.task, id: 5, title: 'Task 5'},
+        {...TaskStories.default.task, id: 6, title: 'Task 6'},
+        {...TaskStories.default.task, id: 7, title: 'Task 7'},
+    ]
+};
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+    tasks: [
+        ...Default.args.tasks.slice(0, 5),
+        { id: 6, title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    ]
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+    tasks: [],
+    loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+    ...Loading.args,
+    loading: false
+};
