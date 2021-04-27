54f085333fb8f7b3328aed5c3302efc13524b0d4
Melvin Idema <melvinidema@melvins-mbp.home>
2021-04-27 16:20:00 +0200

    Add Inbox Screen

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -12,19 +12,22 @@ const Template = args => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
+    onPinTask: () => alert('onPinTask'),
+    onArchiveTask: () => alert('onArchiveTask'),
     tasks: [
-        {...TaskStories.default.task, id: 1, title: 'Task 1'},
-        {...TaskStories.default.task, id: 2, title: 'Task 2'},
-        {...TaskStories.default.task, id: 3, title: 'Task 3'},
-        {...TaskStories.default.task, id: 4, title: 'Task 4'},
-        {...TaskStories.default.task, id: 5, title: 'Task 5'},
-        {...TaskStories.default.task, id: 6, title: 'Task 6'},
-        {...TaskStories.default.task, id: 7, title: 'Task 7'},
+        {...TaskStories.default.task, id: 1, title: 'Task 1', state: 'TASK_INBOX'},
+        {...TaskStories.default.task, id: 2, title: 'Task 2', state: 'TASK_INBOX'},
+        {...TaskStories.default.task, id: 3, title: 'Task 3', state: 'TASK_INBOX'},
+        {...TaskStories.default.task, id: 4, title: 'Task 4', state: 'TASK_INBOX'},
+        {...TaskStories.default.task, id: 5, title: 'Task 5', state: 'TASK_INBOX'},
+        {...TaskStories.default.task, id: 6, title: 'Task 6', state: 'TASK_INBOX'},
+        {...TaskStories.default.task, id: 7, title: 'Task 7', state: 'TASK_INBOX'},
     ]
 };
 
 export const WithPinnedTasks = Template.bind({});
 WithPinnedTasks.args = {
+    ...Default.args,
     tasks: [
         ...Default.args.tasks.slice(0, 5),
         { id: 6, title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
